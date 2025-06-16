afdbf6d5430c5aa6ef6937fd0b69f1e409ddfda1
Hariom <hariomtripathih2001@gmail.com>
2025-06-16 18:34:15 +0530

    lone wolf updating the body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -7,28 +7,10 @@ import { useDispatch } from 'react-redux'
 import { addUserData } from '../utils/userReducer'
 
 const Body = () => {
-    const dispatch=useDispatch()
-
-    useEffect(()=>{
-        userpreivew()
-    },[])
-    const userpreivew=async()=>{
-        try{
-       const res=await axios.get("http://localhost:3000/profile",{withCredentials:true})
-          
-        dispatch(addUserData(res));
-          
-    }
-    catch (e){
-        // e.status(401).message(e.m)
-        console.log(e)
-    }
-    }
+    
   return (
     <div className='h-[80vh]'>
-        <Navbar/>
-        <Outlet/>
-        <Footer/>
+        <p>this is body</p>
     </div>
   )
 }
